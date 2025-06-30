cd2a4c18d70515ffa0c7ac718bcb8b92655fab52
Dávid Šivec <david.sivec20@gmail.com>
2025-06-30 13:14:13 +0200

    funkcna navigacia

--- Autobazár/Frontend/src/Components/Navigation.jsx
+++ Autobazár/Frontend/src/Components/Navigation.jsx
@@ -2,17 +2,52 @@ import { Link } from 'react-router-dom'
 import './Navigation.css'
 
 function Navigation() {
+
+  function sidebar() {
+    const menu = document.querySelector(".menu")
+    const sidebar1 = document.querySelector(".sidebar")
+    let count = 0
+    menu.addEventListener("click", () => {
+      if (count === 0) {
+      sidebar1.style.right = "0"
+      count = 1
+      console.log(count);
+      
+      }
+      if (count === 1) {
+        sidebar1.style.right = "-300px"
+        count = 0
+        console.log(count);
+        
+      }
+    })
+  }  
+
   return (
-    <nav>
-      <div className="logo">CarConnect</div>
-      <div className="links">
-        <Link to="/">Domov</Link>
-        <Link to="/vehicles">Vozidlá</Link>
-        <Link to="/my-advertisements">Moje inzeráty</Link>
-        <Link to="/contact">Kontakt</Link>
-        <Link to="/login">Prihlásenie</Link>
+    <div>
+      <nav>
+        <div className="logo">
+          <Link to="/" className='logo'>CarConnect</Link>
+        </div>
+        <div className="links">
+          <Link to="/vehicles" className='link'>Vozidlá</Link>
+          <Link to="/my-advertisements" className='link'>Moje inzeráty</Link>
+          <Link to="/contact" className='link'>Kontakt</Link>
+          <Link to="/login" className='link'>Prihlásenie</Link>
+          <div className="menu" onClick={sidebar}>
+            <span></span>
+            <span className='span'></span>
+            <span></span>
+          </div>
+        </div>
+      </nav>
+      <div className="sidebar">
+        <Link to="/vehicles" className='line'>Vozidlá</Link>
+        <Link to="/my-advertisements" className='line'>Moje inzeráty</Link>
+        <Link to="/contact" className='line'>Kontakt</Link>
+        <Link to="/login" className='line'>Prihlásenie</Link>
       </div>
-    </nav>
+    </div>
   )
 }
 
