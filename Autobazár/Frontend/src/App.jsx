e924a3d0cb692e4397267b75f3ad95e98e74de7c
Dávid Šivec <david.sivec20@gmail.com>
2025-06-30 10:44:22 +0200

    navigation component + comments

--- Autobazár/Frontend/src/App.jsx
+++ Autobazár/Frontend/src/App.jsx
@@ -1,12 +1,17 @@
+//css imports
 import './App.css'
+//imports components for subpages
 import Vehicles from "./Links/Vehicles"
 import MyAdvertisements from "./Links/MyAdvertisements"
 import Contact from "./Links/Contact"
 import Login from "./Links/Login"
+//imports components for main page
+import Navigation from './Components/Navigation'
 
 function App() {
   return (
     <>
+      <Navigation />
       <h1>This is CarConnect site</h1>
       <Vehicles />
       <MyAdvertisements />
---
+++ Autobazár/Frontend/src/Components/Navigation.jsx
@@ -0,0 +1,12 @@
+import "./Navigation.css"
+
+function Navigation() {
+  return(
+    <nav>
+      <div className="logo">logo</div>
+      <div className="links">links</div>
+    </nav>
+  )
+}
+
+export default Navigation
