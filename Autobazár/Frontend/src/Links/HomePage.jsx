d3479eb95db7f208b32c04ec9cb2bf9a733b44ff
David-Sivec <david.sivec20@gmail.com>
2025-07-02 11:26:53 +0200

    urobene zaklady homepagu

--- Autobazár/Frontend/src/Links/HomePage.jsx
+++ Autobazár/Frontend/src/Links/HomePage.jsx
@@ -1,9 +1,43 @@
 import "./bricks.css"
 
 function HomePage() {
+  //is a platform designed to connect car enthusiasts, buyers, and sellers. Whether you're looking to buy your dream car, sell your vehicle, or simply explore the automotive world, CarConnect has you covered.
   return(
     <div className="homepage">
-      Home
+      <div className="wrapper">
+        <div className="introduction">
+          <h1>CarConnect</h1>
+          <p className="text">Buy and sell cars easily with our community-driven online marketplace.</p>
+          <div className="filtration">
+            hi
+          </div>
+        </div>
+        <div className="best-advertisements">
+          hi
+        </div>
+        <div className="cards">
+          <div className="card">hi</div>
+          <div className="card">hi</div>
+          <div className="card">hi</div>
+          <div className="card">hi</div>
+        </div>
+        <div className="add-advertisement">
+          hi
+        </div>
+        <div className="infinite-scroll">
+          hi
+        </div>
+        <h2>Recensions</h2>
+        <div className="recensions">
+          <div className="recension">hi</div>
+          <div className="recension">hi</div>
+        </div>
+        <h2>Contact formular</h2>
+        <div className="contact"></div>
+      </div>
+      <div className="footer">
+        footer
+      </div>
     </div>
   )
 }
