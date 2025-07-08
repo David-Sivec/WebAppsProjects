ab75ac4184237a323c51123ad329f0c796d0c08e
David-Sivec <david.sivec20@gmail.com>
2025-07-08 13:56:43 +0200

    infinite scroll

--- Autobazár/Frontend/src/Components/InfiniteScroll.jsx
+++ Autobazár/Frontend/src/Components/InfiniteScroll.jsx
@@ -3,7 +3,27 @@ import "./InfiniteScroll.css"
 function InfiniteScroll() {
   return (
     <div className="infinite-scroll">
-      hi
+      <div className="brands">
+        <div className="brand b1"></div>
+        <div className="brand b2"></div>
+        <div className="brand b3"></div>
+        <div className="brand b4"></div>
+        <div className="brand b5"></div>
+      </div>
+      <div className="brands">
+        <div className="brand2 b1"></div>
+        <div className="brand2 b2"></div>
+        <div className="brand2 b3"></div>
+        <div className="brand2 b4"></div>
+        <div className="brand2 b5"></div>
+      </div>
+      <div className="brands">
+        <div className="brand b1"></div>
+        <div className="brand b2"></div>
+        <div className="brand b3"></div>
+        <div className="brand b4"></div>
+        <div className="brand b5"></div>
+      </div>
     </div>
   )
 }
