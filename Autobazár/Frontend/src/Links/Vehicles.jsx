5f4df52a0d1fcc2306e1086e9ab0645a18055db6
Dávid Šivec <david.sivec20@gmail.com>
2025-07-15 12:37:24 +0200

    vehicle subpage

--- Autobazár/Frontend/src/Links/Vehicles.jsx
+++ Autobazár/Frontend/src/Links/Vehicles.jsx
@@ -12,7 +12,20 @@ function Vehicles() {
         <div className="box">
           <HorizontalFiltration />
           <div className="vehicle-list">
-            <div className="vehicle-card">hi</div>
+            <div className="vehicle-card">
+              <img src="./images/bg.jpg" alt="" className="vehicle-card-image" />
+              <div className="location">
+                Slovakia
+                <svg width="40px" height="40px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M12 21C15.5 17.4 19 14.1764 19 10.2C19 6.22355 15.866 3 12 3C8.13401 3 5 6.22355 5 10.2C5 14.1764 8.5 17.4 12 21Z" stroke="#98CDFF" stroke-width="1.44" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M12 13C13.6569 13 15 11.6569 15 10C15 8.34315 13.6569 7 12 7C10.3431 7 9 8.34315 9 10C9 11.6569 10.3431 13 12 13Z" stroke="#98CDFF" stroke-width="1.44" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+              </div>
+              <h3>Skoda Octavia</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <p>2015 <span>·</span> 88000km <span>·</span> combi <span>·</span> used <span>·</span> petrol <span>·</span> 8L/100km</p>
+              <div className="vehicle-card-bottom">
+                <p className="card-price">€20,000</p>
+                <button className="vehicle-card-btn">Show more</button>
+              </div>
+            </div>
             <div className="vehicle-card">hi</div>
             <div className="vehicle-card">hi</div>
             <div className="vehicle-card">hi</div>
