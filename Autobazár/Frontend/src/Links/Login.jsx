8c5526c0912a71fbdaaa357dbe9cbd53d8e82271
Dávid Šivec <david.sivec20@gmail.com>
2025-07-18 13:47:52 +0200

    login subpage

--- Autobazár/Frontend/src/Links/Login.jsx
+++ Autobazár/Frontend/src/Links/Login.jsx
@@ -1,6 +1,13 @@
 import "./bricks.css"
+import { useState } from "react"
 
 function Login() {
+  const [switchLogin, setSwitchLogin] = useState(false)
+
+  const switchBettwen = () => {
+    setSwitchLogin(!switchLogin)
+  }
+
   return (
     <div className="login">
       <div className="login-container">
@@ -29,7 +36,7 @@ function Login() {
               <label>Enter your location
                 <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M12 14.5V16.5M7 10.0288C7.47142 10 8.05259 10 8.8 10H15.2C15.9474 10 16.5286 10 17 10.0288M7 10.0288C6.41168 10.0647 5.99429 10.1455 5.63803 10.327C5.07354 10.6146 4.6146 11.0735 4.32698 11.638C4 12.2798 4 13.1198 4 14.8V16.2C4 17.8802 4 18.7202 4.32698 19.362C4.6146 19.9265 5.07354 20.3854 5.63803 20.673C6.27976 21 7.11984 21 8.8 21H15.2C16.8802 21 17.7202 21 18.362 20.673C18.9265 20.3854 19.3854 19.9265 19.673 19.362C20 18.7202 20 17.8802 20 16.2V14.8C20 13.1198 20 12.2798 19.673 11.638C19.3854 11.0735 18.9265 10.6146 18.362 10.327C18.0057 10.1455 17.5883 10.0647 17 10.0288M7 10.0288V8C7 5.23858 9.23858 3 12 3C14.7614 3 17 5.23858 17 8V10.0288" stroke="#ffffff" stroke-width="1.44" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
               </label>
-              <select>
+              <select className="selection">
                 <option value="">1</option>
                 <option value="">2</option>
                 <option value="">3</option>
@@ -47,6 +54,12 @@ function Login() {
             Register
           </button>
         </section>
+        <div className={`switch-hider ${switchLogin ? "active-switch" : ""}`}>
+          {switchLogin ? "Already have an account? log in here" : "Dont have an account? register here"}
+          <div className="login-switch-btn" onClick={switchBettwen}>
+            {switchLogin ? "log in" : "register"}
+          </div>
+        </div>
         <section className="login-section">
           <h2>Sign in to your account</h2>
           <div className="login-input-wrapper">
