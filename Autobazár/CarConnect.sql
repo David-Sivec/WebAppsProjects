d7e9480e045102fe180848cedf62c696427519e1
Dávid Šivec <david.sivec20@gmail.com>
2025-07-19 11:54:44 +0200

    database

--- Autobazár/CarConnect.sql
+++ Autobazár/CarConnect.sql
@@ -16,9 +16,23 @@ CREATE TABLE formularmessages(
 )
 
 CREATE TABLE advertisements(
-	id_advertisement	SERIAL		PRIMARY KEY,
-	car_brand			VARCHAR(50),
-	car_model			VARCHAR(50),
-	car_location		VARCHAR(50),
-	-- not finished--
+	id_advertisement			SERIAL		PRIMARY KEY,
+	advertisement_brand			VARCHAR(50),
+	advertisement_model			VARCHAR(50),
+	advertisement_location		VARCHAR(50),
+	advertisement_text			VARCHAR(200),
+	advertisement_image			VARCHAR(100),
+	advertisement_year			INT			NOT NULL,
+	advertisement_fuel			VARCHAR(20),
+	advertisement_consumption	VARCHAR(20),
+	advertisement_type			VARCHAR(50),
+	advertisement_status		VARCHAR(20),
+	advertisement_user			VARCHAR(100)
+)
+
+CREATE TABLE recensions(
+	id_recension		SERIAL		PRIMARY KEY,
+	user_name			VARCHAR(100),
+	user_text			VARCHAR(300),
+	user_rating			VARCHAR(5)
 )
